122386bf220e9e05152d79d44b5f5099a7e8c7a4
Andimitar <dababung.us@gmail.com>
2026-03-10 10:49:27 +0200

    helo

---
+++ .idea/neshto/neshto.cpp
@@ -0,0 +1,15 @@
+int main(){
+
+    // you're welcome vro :)
+
+    System.out.println("Maraba brat!");
+
+    String greeting = "Maraba brat!"
+
+    for(int i = 0; i < 5; i++) {
+        System.out.printl(greeting);
+    }
+
+
+//how you know im dominican baabii
+}
